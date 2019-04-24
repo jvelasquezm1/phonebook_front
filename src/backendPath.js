234d55db057b7c03d5b2d2d14d2d3c6253ff953a
Juanes Velasquez <jvelasquezm@psl.com.co>
2019-04-24 08:52:59 -0500

    create components

---
+++ src/backendPath.js
@@ -0,0 +1,3 @@
+const backendPath = { ROOT_URL: 'http://localhost:3000' };
+
+export default backendPath;
---
+++ src/components/CreateForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create</h1>
+      </div>
+    )
+  }
+}
