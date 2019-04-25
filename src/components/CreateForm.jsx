4187e1fec1dbedb9bf08dc72f509424ec4471360
Juanes Velasquez <jvelasquezm@psl.com.co>
2019-04-25 16:26:39 -0500

    use sweetalert

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import Header from './header/Header'
 import styles from './components.module.scss'
 
-import { connect } from 'react-redux';
-import EntriesService from '../services/entries.services';
-import * as EntriesAction from '../store/actions/entries.actions';
+import { connect } from 'react-redux'
+import EntriesService from '../services/entries.services'
+import * as EntriesAction from '../store/actions/entries.actions'
+import SweetAlert from 'sweetalert-react'
 
 class CreateForm extends Component {
   state = {
@@ -14,69 +15,88 @@ class CreateForm extends Component {
     entries: {},
     error: '',
     isValid: false,
+    success: false,
+    successMessage: ''
   }
 
   isValidForm = (firstName, lastName) => {
-    const isValid = firstName !== '' && lastName !== '';
+    const isValid = firstName !== '' && lastName !== ''
     if (!isValid) {
       this.setState({
         error: 'Please provide a first and last name',
-        isValid: false,
-      });
+        isValid: false
+      })
     } else {
       this.setState({
         error: null,
-        isValid: true,
-      });
+        isValid: true
+      })
     }
   }
 
-  isValidPhone = (phone) => {
+  isValidPhone = phone => {
     console.log(phone.length)
-    const isValid = phone.match(/^\+\d.{1,1}.\d.{1,1}.\d+/) && phone.length > 12;
+    const isValid = phone.match(/^\+\d.{1,1}.\d.{1,1}.\d+/) && phone.length > 12
     if (!isValid) {
       this.setState({
         error: 'The format for the number must follow "+11 11 111111..."',
-        isValid: false,
-      });
+        isValid: false
+      })
     } else {
       this.setState({
         error: null,
-        isValid: true,
-      });
+        isValid: true
+      })
     }
   }
-  
-  onChangeFirstName = (event) => {
-    const { firstName, lastName } = this.state;    
-    this.setState({ firstName: event.target.value });
-    this.isValidForm(firstName, lastName);
+
+  onChangeFirstName = event => {
+    const { firstName, lastName } = this.state
+    this.setState({ firstName: event.target.value })
+    this.isValidForm(firstName, lastName)
   }
 
-  onChangeLastName = (event) => {
-    const { firstName, lastName } = this.state;
-    this.setState({ lastName: event.target.value });
-    this.isValidForm(firstName, lastName);
+  onChangeLastName = event => {
+    const { firstName, lastName } = this.state
+    this.setState({ lastName: event.target.value })
+    this.isValidForm(firstName, lastName)
   }
 
-  onChangePhone = (event) => {
-    this.setState({ phone: event.target.value });
-    this.isValidPhone(event.target.value);
+  onChangePhone = event => {
+    this.setState({ phone: event.target.value })
+    this.isValidPhone(event.target.value)
   }
 
-  createEntry = async () => {    
-    const { firstName, lastName, phone } = this.state;
-    const entries = { firstName, lastName, phone };
+  createEntry = async () => {
+    const { firstName, lastName, phone } = this.state
+    const entries = { firstName, lastName, phone }
     try {
-      await EntriesService.addEntries(entries);
-      console.log('ok')
+      await EntriesService.addEntries(entries)
+      this.setState({
+        success: true,
+        successMessage: 'Entry successfully created'
+      })
     } catch (error) {
       console.log(error)
     }
   }
 
+  closeAlert = () => {
+    this.setState({ success: false, successMessage: '' });
+    let path = '/';
+    this.props.history.push(path);
+  }
+
   render () {
-    const { firstName, lastName, phone, isValid, error } = this.state;
+    const {
+      firstName,
+      lastName,
+      phone,
+      isValid,
+      error,
+      success,
+      successMessage
+    } = this.state
     return (
       <div>
         <Header>
@@ -88,23 +108,50 @@ class CreateForm extends Component {
             <h3>Create entry:</h3>
             <div className={styles.InputGroup}>
               <div>First Name</div>
-              <input className='form-control' placeholder='First Name' value={ firstName } onChange={this.onChangeFirstName} />
+              <input
+                className='form-control'
+                placeholder='First Name'
+                value={firstName}
+                onChange={this.onChangeFirstName}
+              />
             </div>
             <div className={styles.InputGroup}>
               <div>Last Name</div>
-              <input className='form-control' placeholder='Last Name' value={ lastName } onChange={this.onChangeLastName} />
+              <input
+                className='form-control'
+                placeholder='Last Name'
+                value={lastName}
+                onChange={this.onChangeLastName}
+              />
             </div>
             <div className={styles.InputGroup}>
               <div>Phone</div>
-              <input className='form-control' placeholder='Phone' value={ phone } onChange={this.onChangePhone} />
+              <input
+                className='form-control'
+                placeholder='Phone'
+                value={phone}
+                onChange={this.onChangePhone}
+              />
             </div>
-            {!isValid &&
-            <div className={styles.InputGroup}>
-              <div>{error}</div>
-            </div>}
-            <button type='button' className={`${styles.btn} ${styles.btn_primary}`} onClick={this.createEntry}>
+            {!isValid && (
+              <div className={styles.InputGroup}>
+                <div>{error}</div>
+              </div>
+            )}
+            <button
+              type='button'
+              className={`${styles.btn} ${styles.btn_primary}`}
+              onClick={this.createEntry}
+            >
               {'Create'}
             </button>
+            <SweetAlert
+              show={success}
+              title='Success'
+              text={successMessage}
+              type='success'
+              onConfirm={() => this.closeAlert()}
+            />
           </div>
         </form>
       </div>
@@ -114,14 +161,14 @@ class CreateForm extends Component {
 
 const mapStateToProps = state => ({
   entries: state.entries.entries,
-  isFetchingEntries: state.entries.isFetchingEntries,
-});
+  isFetchingEntries: state.entries.isFetchingEntries
+})
 
 const mapDispatchToProps = dispatch => ({
-  fetchEntries: () => dispatch(EntriesAction.fetchEntries()),
-});
+  fetchEntries: () => dispatch(EntriesAction.fetchEntries())
+})
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(CreateForm);
+  mapDispatchToProps
+)(CreateForm)
--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -2,58 +2,75 @@ import React, { Component } from 'react'
 import Header from './header/Header'
 import styles from './components.module.scss'
 
-import { connect } from 'react-redux';
-import EntriesService from '../services/entries.services';
-import * as EntriesAction from '../store/actions/entries.actions';
+import { connect } from 'react-redux'
+import EntriesService from '../services/entries.services'
+import * as EntriesAction from '../store/actions/entries.actions'
+import SweetAlert from 'sweetalert-react'
 
 class EditForm extends Component {
   state = {
-    entryId:  window.localStorage.entryId,
+    entryId: window.localStorage.entryId,
     firstName: '',
     lastName: '',
     phone: '',
-  }
-  
-  onChangeFirstName = (event) => {
-    this.setState({ firstName: event.target.value });
+    success: false,
+    successMessage: ''
   }
 
-  onChangeLastName = (event) => {
-    this.setState({ lastName: event.target.value });
+  onChangeFirstName = event => {
+    this.setState({ firstName: event.target.value })
   }
 
-  onChangePhone = (event) => {
-    this.setState({ phone: event.target.value });
+  onChangeLastName = event => {
+    this.setState({ lastName: event.target.value })
+  }
+
+  onChangePhone = event => {
+    this.setState({ phone: event.target.value })
   }
 
   componentDidMount = () => {
-    this.getEntryById();
+    this.getEntryById()
   }
 
   getEntryById = async () => {
-    const { entryId } = this.state;
+    const { entryId } = this.state
     try {
-      const entry = await EntriesService.getEntryById(entryId);
-      const { firstName, lastName, phone } = entry;
+      const entry = await EntriesService.getEntryById(entryId)
+      const { firstName, lastName, phone } = entry
       this.setState({ firstName, lastName, phone })
     } catch (error) {
       console.log(error)
     }
   }
 
-  updateEntry = async () => {    
-    const { firstName, lastName, phone } = this.state;
-    const entries = { firstName, lastName, phone, id: window.localStorage.entryId };
+  updateEntry = async () => {
+    const { firstName, lastName, phone } = this.state
+    const entries = {
+      firstName,
+      lastName,
+      phone,
+      id: window.localStorage.entryId
+    }
     try {
-      await EntriesService.editEntries(entries);
-      console.log('ok')
+      await EntriesService.editEntries(entries)
+      this.setState({
+        success: true,
+        successMessage: 'Entry successfully updated'
+      })
     } catch (error) {
       console.log(error)
     }
   }
 
+  closeAlert = () => {
+    this.setState({ success: false, successMessage: '' });
+    let path = '/';
+    this.props.history.push(path);
+  }
+
   render () {
-    const { firstName, lastName, phone } = this.state;
+    const { firstName, lastName, phone, success, successMessage } = this.state
     return (
       <div>
         <Header>
@@ -65,19 +82,42 @@ class EditForm extends Component {
             <h3>Edit entry:</h3>
             <div className={styles.InputGroup}>
               <div>First Name</div>
-              <input className='form-control' value={firstName} onChange={this.onChangeFirstName} />
+              <input
+                className='form-control'
+                value={firstName}
+                onChange={this.onChangeFirstName}
+              />
             </div>
             <div className={styles.InputGroup}>
               <div>Last Name</div>
-              <input className='form-control' value={lastName} onChange={this.onChangeLastName} />
+              <input
+                className='form-control'
+                value={lastName}
+                onChange={this.onChangeLastName}
+              />
             </div>
             <div className={styles.InputGroup}>
               <div>Phone</div>
-              <input className='form-control' value={phone} onChange={this.onChangePhone} />
+              <input
+                className='form-control'
+                value={phone}
+                onChange={this.onChangePhone}
+              />
             </div>
-            <button type='button' className={`${styles.btn} ${styles.btn_primary}`} onClick={this.updateEntry}>
+            <button
+              type='button'
+              className={`${styles.btn} ${styles.btn_primary}`}
+              onClick={this.updateEntry}
+            >
               {'Update'}
             </button>
+            <SweetAlert
+              show={success}
+              title='Success'
+              text={successMessage}
+              type='success'
+              onConfirm={() => this.closeAlert()}
+            />
           </div>
         </form>
       </div>
@@ -87,14 +127,14 @@ class EditForm extends Component {
 
 const mapStateToProps = state => ({
   entries: state.entries.entries,
-  isFetchingEntries: state.entries.isFetchingEntries,
-});
+  isFetchingEntries: state.entries.isFetchingEntries
+})
 
 const mapDispatchToProps = dispatch => ({
-  fetchEntries: () => dispatch(EntriesAction.fetchEntries()),
-});
+  fetchEntries: () => dispatch(EntriesAction.fetchEntries())
+})
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(EditForm);
+  mapDispatchToProps
+)(EditForm)
